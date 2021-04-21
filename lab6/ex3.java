710ddedb7b232fb93745a5174de0a274881a6e43
utkarshb29 <65757859+utkarshb29@users.noreply.github.com>
2021-04-21 20:54:36 +0530

    Update ex3.java

--- lab6/ex3.java
+++ lab6/ex3.java
@@ -1,5 +1,18 @@
 package lab6;
-
+import java.util.HashMap;
+import java.util.Map;
 public class ex3 {
 
+	public static void main(String[] args) {
+		int[] num= {1,3,4,6,2,7,8};
+		System.out.println(getSquares(num));
+	}
+	private static Map<Integer, Long> getSquares(int[] n) {
+		Map<Integer, Long> mymap=new HashMap<Integer, Long>();
+		for(int a:n) {
+			long sqr=a*a;
+			mymap.put(a, sqr);
+		}
+		return mymap;
+	}
 }
