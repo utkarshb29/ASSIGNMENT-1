895c8f46e5a983cc5df22b27655cfb00dda5e752
utkarshb29 <65757859+utkarshb29@users.noreply.github.com>
2021-04-21 20:52:34 +0530

    Update ex5.java

--- lab6/ex5.java
+++ lab6/ex5.java
@@ -1,5 +1,33 @@
 package lab6;
-
+import java.util.*;
 public class ex5 {
+public static int getSecondSmallest(int[] a) {
+		
+		List<Integer> l = new ArrayList<Integer>();
+		
+		for(int i = 0; i < a.length; i++) {
+			l.add(a[i]);
+		}
+		
+		Collections.sort(l);
+		return l.get(1);
+	}
 
+	public static void main(String[] args) {
+		while(true) {
+		System.out.println("Enter Arr here:");
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for(int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+		System.out.println("Second Smallest Integer is:");
+		System.out.println("______________________________________");
+		System.out.println(getSecondSmallest(a));
+		}
+	}
+
+}
 }
